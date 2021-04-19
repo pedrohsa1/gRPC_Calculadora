81d5181c7813be858c662b2369be0f7508d85b24
Pedro <pedro.si.dev@gmail.com>
2021-04-19 16:08:40 -0300

    Projeto calculadora RPC

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var calculadoaClient = new Calculadora.CalculadoraClient(channel);
+
+            Console.Write("Digite o N1: ");
+            int n1 = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Digite o N2: ");
+            int n2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Termo 1: { n1 }"); 
+            Console.WriteLine($"Termo 2: { n2 }");
+
+            var clientRequested = new CalculadoraLookupModel { 
+                N1 = n1, 
+                N2 = n2
+            };
+
+            var calculadora = await calculadoaClient.GetCalculadoraInfoAsync(clientRequested);
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("### Servidor ###");
+            Console.WriteLine("Soma: ");
+            Console.WriteLine($"{ calculadora.N1 } + { calculadora.N2 } = {calculadora.Soma}");
+            Console.WriteLine("Subtração: ");
+            Console.WriteLine($"{ calculadora.N1 } - { calculadora.N2 } = {calculadora.Subtracao}");
+            Console.WriteLine("Multiplicação: ");
+            Console.WriteLine($"{ calculadora.N1 } * { calculadora.N2 } = {calculadora.Multiplicacao}");
+            Console.WriteLine("Divisão: ");
+            Console.WriteLine($"{ calculadora.N1 } / { calculadora.N2 } = {calculadora.Divisao}");
+
+            Console.WriteLine();
+            Console.WriteLine("Novo Calculo");
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcServer/Services/CalculadoraServices.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CalculadoraService : Calculadora.CalculadoraBase
+    {
+        private readonly ILogger<CalculadoraService> _logger;
+        public CalculadoraService(ILogger<CalculadoraService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CalculadoraModel> GetCalculadoraInfo(CalculadoraLookupModel request, ServerCallContext context)
+        {
+            CalculadoraModel output = new CalculadoraModel();
+
+            output.N1 = request.N1;
+            output.N2 = request.N2;
+            output.Soma = output.N1 + output.N2;
+            output.Subtracao = output.N1 - output.N2;
+            output.Multiplicacao = output.N1 * output.N2;
+            output.Divisao = output.N1 / output.N2;
+
+            return Task.FromResult(output);
+        }
+    }
+}
